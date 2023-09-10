7d947997d951dd9efb23d67c5dc2864955a9ba60
Kshitij <kshitij.tanwar2002@gmail.com>
2023-09-11 02:05:59 +0530

    first

--- src/Components/store/CartProvider.jsx
+++ src/Components/store/CartProvider.jsx
@@ -10,7 +10,7 @@ const cartReducer = (state, action) => {
     if (action.type === "ADD") {
         const updatedItems = state.items.concat(action.item);
         const updatedTotalAmount =
-            state.totalAmount + action.item.price * action.item.amount;
+            state.totalAmount + action.item.price * action.item.quantity;
         return {
             items: updatedItems,
             totalAmount: updatedTotalAmount,
@@ -18,6 +18,7 @@ const cartReducer = (state, action) => {
     }
     return defaultCartState;
 };
+
 const CartProvider = (props) => {
     const [cartState, dispatchCartAction] = useReducer(
         cartReducer,
